0555e08e3abe07411f4656d934749ef74d1ae4c1
AlexSlate7 <asmatveyev007@gmail.com>
2023-07-24 18:40:46 +0200

    игрок двигается и управляется с джойстика

---
+++ Assets/Code/Player/PlayerMove.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using Code.Infrastructure;
+using Code.Services.Input;
+
+namespace Code.Player
+{
+
+    public class PlayerMove : MonoBehaviour
+    {
+        public CharacterController CharacterController;
+        public float MovementSpeed;
+
+        private IInputService _inputService;
+        private Camera _camera;
+
+        private void Awake()
+        {
+            _inputService = Game.InputService;
+        }
+
+        private void Start()
+        {
+            _camera = Camera.main;
+        }
+
+        private void Update()
+        {
+            Vector3 movementVector = Vector3.zero;
+
+            if (_inputService.Axis.sqrMagnitude > 0.001)
+            {
+                movementVector = _camera.transform.TransformDirection(_inputService.Axis);
+                movementVector.Normalize();
+            }
+            CharacterController.Move(MovementSpeed * movementVector * Time.deltaTime);
+        }
+    }
+}
+
