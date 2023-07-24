ace4c50440b0ab7575f5643ea2768fef638fced1
AlexSlate7 <asmatveyev007@gmail.com>
2023-07-24 17:25:16 +0200

    почистил проект, добавил zenject

---
+++ Assets/Code/Player/PlayerMoving.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoving : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
