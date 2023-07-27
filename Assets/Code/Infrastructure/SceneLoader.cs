2e2d383cf16037e335c21ddb9a23688a8744563a
AlexSlate7 <asmatveyev007@gmail.com>
2023-07-27 17:08:22 +0200

    создал initial сцену и загрузчик сцен

---
+++ Assets/Code/Infrastructure/SceneLoader.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+namespace Code.Infrastructure
+{
+    public class SceneLoader
+    {
+        private readonly ICoroutineRunner _coroutineRunner;
+
+        public SceneLoader(ICoroutineRunner coroutineRunner) =>
+            _coroutineRunner = coroutineRunner;
+
+        public void Load(string name, Action onLoaded = null) =>
+            _coroutineRunner.StartCoroutine(LoadScene(name, onLoaded));
+
+        private IEnumerator LoadScene(string name, Action onLoaded = null)
+        {
+            AsyncOperation waitNextScene = SceneManager.LoadSceneAsync(name);
+
+            while (waitNextScene.isDone)
+            {
+                yield return null;
+            }
+
+            onLoaded?.Invoke();
+        }
+    }
+    
+}
+
